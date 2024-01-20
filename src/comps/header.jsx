5cfd143ff1abba12d8be3cfdc819ae8931f29ad7
apmanager <rfoley00@live.com>
2024-01-20 01:32:05 -0500

    updating header

--- src/comps/header.jsx
+++ src/comps/header.jsx
@@ -1,42 +1,69 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import './css/header.css'
+import React, { useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faBars } from '@fortawesome/free-solid-svg-icons';
+import styles from './css/header.module.css';
 
 const Header = () => {
+  const navRef = useRef(null);
+
+  useEffect(() => {
+    const menuIcon = document.getElementById('toggleMenu');
+
+    const handleMenuToggle = () => {
+      if (navRef.current) {
+        navRef.current.style.display = navRef.current.style.display === 'block' ? 'none' : 'block';
+      }
+      menuIcon.classList.toggle(styles.open);
+    };
+
+    menuIcon.addEventListener('click', handleMenuToggle);
+
+    return () => {
+      menuIcon.removeEventListener('click', handleMenuToggle);
+    };
+  }, []);
+
   return (
-        <nav className="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-            <div className="container-fluid" >
-                <img src="./logo.svg" class="card-img-top" alt="Logo" style={{width: "100px"}}/>
-                <h1><Link className="navbar-brand title" to="/">Movie Chat</Link></h1>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse links" id="navbarNavDropdown">
-                <ul className="navbar-nav ">
-                    <li className="nav-item">
-                    <Link className="nav-link active" aria-current="page" to='/'>Home</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link className="nav-link" to="/about">Movies</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link className="nav-link" to="/about">About</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link to='/contact' className="nav-link" href="#">Contact</Link>
-                    </li>
-                    <li className="nav-item ">
-                    <Link className="nav-link" to="/signIn">Sign In</Link>
-                    </li>
-                    <li className="nav-item ">
-                    <Link className="nav-link" to="/test">Test</Link>
-                    </li>
-                   
-                </ul>
-                </div>
-            </div>
-        </nav>
-  )
-}
+    <>
+      <div className={styles.navContainer}>
+        <div className={styles.logo}>
+          <img src="./logo.svg" alt="Logo" />
+        </div>
+        <div className={styles.title}>
+          <Link to='/'>Movie Chat</Link>
+        </div>
+
+        <div className={styles.hamburger}>
+          <a className={styles.icons} id='toggleMenu'>
+            <FontAwesomeIcon icon={faBars} />
+          </a>
+        </div>
+        
+          <div className={`${styles.home} ${styles.mobileLinks}`}>
+            <Link to='/'>Home</Link>
+          </div>
+          <div className={`${styles.movies} ${styles.mobileLinks}`}>
+            <Link to="/about">Movies</Link>
+          </div>
+          <div className={`${styles.about} ${styles.mobileLinks}`}>
+            <Link to="/about">About</Link>
+          </div>
+          <div className={`${styles.contact} ${styles.mobileLinks}`}>
+            <Link to='/contact' >Contact</Link>
+          </div>
+          <div className={`${styles.account} ${styles.mobileLinks}`}>
+            <Link to="/signIn"><FontAwesomeIcon className={styles.icon} icon={faUser} />
+              Account
+            </Link>
+          </div>
+          <div className={`${styles.test} ${styles.mobileLinks}`}>
+            <Link to="/test">Test</Link>
+          </div>
+        
+      </div>
+    </>
+  );
+};
 
 export default Header;
