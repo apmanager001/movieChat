d9ac4830b88e99b554d40f4e61979de8b6f2b1c1
Forrest Goryl <forrestgoryl@outlook.com>
2024-01-31 09:46:27 -0600

    Migrate db changes

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('users', views.get_users, name='users'),
   path('signIn', views.sign_in_view, name='sign_in'),
   path('createAcct', views.create_account_view, name='create_account'),
 ]
