7b016a48cc75050f20f3b57a232dee0adc3d8610
Forrest Goryl <forrestgoryl@outlook.com>
2024-02-06 15:26:20 -0600

    Add api comp. Modify signin page to handle signIn and createAcct route

--- src/comps/pages/signIn.jsx
+++ src/comps/pages/signIn.jsx
@@ -1,13 +1,44 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import api from '../api';
 import styles from '../css/signIn.module.css';
 import Header from '../header';
 import Footer from '../footer';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faGoogle, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import {
+  faFacebook,
+  faGoogle,
+  faGithub,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
 
 const SignIn = () => {
   const [isSignIn, setSignIn] = useState(true);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignIn = async (event) => {
+    event.preventDefault();
+    console.log('Trying post request');
+    try {
+      const response = await api.post('signIn', { email, password });
+      console.log('Response status from signIn: ', response.status);
+    } catch (error) {
+      console.log('Caught error\n', error.response);
+    }
+  };
+
+  const handleCreateAcct = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await api.post('createAcct', { name, email, password });
+      console.log('Response status from createAcct: ', response.status);
+    } catch (error) {
+      console.log('Caught error\n', error.response);
+    }
+  };
 
   const handleToggleClick = () => {
     setSignIn((prev) => !prev);
@@ -19,58 +50,157 @@ const SignIn = () => {
 
       <div className={styles.signInContainer}>
         <div className={styles.container}>
-          <div className={`${styles.formContainer} ${isSignIn ? styles.signIn : styles.signUp}`}>
-            <form>
+          <div
+            className={`${styles.formContainer} ${
+              isSignIn ? styles.signIn : styles.signUp
+            }`}
+          >
+            <form onSubmit={isSignIn ? handleSignIn : handleCreateAcct}>
               <h1>{isSignIn ? 'Sign In' : 'Create Account'}</h1>
               <div className={styles.socialIcons}>
-                <Link to='/' className={styles.icon}><FontAwesomeIcon icon={faGoogle} /></Link>
-                <Link to='/' className={styles.icon}><FontAwesomeIcon icon={faFacebook} /></Link>
-                <Link to='/' className={styles.icon}><FontAwesomeIcon icon={faGithub} /></Link>
-                <Link to='/' className={styles.icon}><FontAwesomeIcon icon={faLinkedin} /></Link>
+                <Link
+                  to='/'
+                  className={styles.icon}
+                >
+                  <FontAwesomeIcon icon={faGoogle} />
+                </Link>
+                <Link
+                  to='/'
+                  className={styles.icon}
+                >
+                  <FontAwesomeIcon icon={faFacebook} />
+                </Link>
+                <Link
+                  to='/'
+                  className={styles.icon}
+                >
+                  <FontAwesomeIcon icon={faGithub} />
+                </Link>
+                <Link
+                  to='/'
+                  className={styles.icon}
+                >
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </Link>
               </div>
               {isSignIn ? (
                 <>
                   <span>or use your email and password</span>
-                  <input type="text" placeholder="Email" />
-                  <input type="password" placeholder="Password" />
+                  <input
+                    type='text'
+                    placeholder='Email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <input
+                    type='password'
+                    placeholder='Password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
                   <Link to='/'>Forgot Your Password?</Link>
-                  <button>Sign In</button>
+                  <button type='submit'>Sign In</button>
                 </>
               ) : (
                 <>
                   <span>or use your email to register</span>
-                  <input type="text" placeholder="Name" />
-                  <input type="text" placeholder="Email" />
-                  <input type="password" placeholder="Password" />
-                  <button>Sign Up</button>
+                  <input
+                    type='text'
+                    placeholder='Name'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                  <input
+                    type='text'
+                    placeholder='Email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <input
+                    type='password'
+                    placeholder='Password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <button type='submit'>Sign Up</button>
                 </>
               )}
             </form>
           </div>
-          <div className={`${styles.formContainer} ${!isSignIn ? ' ' + styles.signIn : ''}`}>
-            <form>
+          <div
+            className={`${styles.formContainer} ${
+              !isSignIn ? ' ' + styles.signIn : ''
+            }`}
+          >
+            <form onSubmit={isSignIn ? handleSignIn : handleCreateAcct}>
               <h1>{isSignIn ? 'Sign In' : 'Create Account'}</h1>
               <div className={styles.socialIcons}>
-                <Link to='/' className={styles.icon}><FontAwesomeIcon icon={faGoogle} /></Link>
-                <Link to='/' className={styles.icon}><FontAwesomeIcon icon={faFacebook} /></Link>
-                <Link to='/' className={styles.icon}><FontAwesomeIcon icon={faGithub} /></Link>
-                <Link to='/' className={styles.icon}><FontAwesomeIcon icon={faLinkedin} /></Link>
+                <Link
+                  to='/'
+                  className={styles.icon}
+                >
+                  <FontAwesomeIcon icon={faGoogle} />
+                </Link>
+                <Link
+                  to='/'
+                  className={styles.icon}
+                >
+                  <FontAwesomeIcon icon={faFacebook} />
+                </Link>
+                <Link
+                  to='/'
+                  className={styles.icon}
+                >
+                  <FontAwesomeIcon icon={faGithub} />
+                </Link>
+                <Link
+                  to='/'
+                  className={styles.icon}
+                >
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </Link>
               </div>
               {isSignIn ? (
                 <>
                   <span>or use your email and password</span>
-                  <input type="text" placeholder="Email" />
-                  <input type="password" placeholder="Password" />
+                  <input
+                    type='text'
+                    placeholder='Email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <input
+                    type='text'
+                    placeholder='Password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
                   <div className={styles.forgotPassword}>
-                  <Link  to='/'>Forgot Your Password?</Link></div>
+                    <Link to='/'>Forgot Your Password?</Link>
+                  </div>
                   <button>Sign In</button>
                 </>
               ) : (
                 <>
                   <span>or use your email to register</span>
-                  <input type="text" placeholder="Name" />
-                  <input type="text" placeholder="Email" />
-                  <input type="password" placeholder="Password" />
+                  <input
+                    type='text'
+                    placeholder='Name'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                  <input
+                    type='text'
+                    placeholder='Email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <input
+                    type='text'
+                    placeholder='Password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
                   <button>Sign Up</button>
                 </>
               )}
@@ -79,10 +209,22 @@ const SignIn = () => {
 
           <div className={styles.toggleContainer}>
             <div className={styles.toggle}>
-              <div className={`${styles.togglePanel} ${isSignIn ? styles.toggleLeft : styles.toggleRight}`}>
+              <div
+                className={`${styles.togglePanel} ${
+                  isSignIn ? styles.toggleLeft : styles.toggleRight
+                }`}
+              >
                 <h1>{isSignIn ? 'Welcome Back!' : 'Get Started!'}</h1>
-                <p>{isSignIn ? 'Already have an account? Click here to log in! ': 'Already have an account? Click here to Sign In!'}</p>
-                <button id={styles.toggleButton} className={styles.hidden} onClick={handleToggleClick}>
+                <p>
+                  {isSignIn
+                    ? 'Already have an account? Click here to log in! '
+                    : 'Already have an account? Click here to Sign In!'}
+                </p>
+                <button
+                  id={styles.toggleButton}
+                  className={styles.hidden}
+                  onClick={handleToggleClick}
+                >
                   {isSignIn ? 'Create Your Account' : 'Sign In'}
                 </button>
               </div>
