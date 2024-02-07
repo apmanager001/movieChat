79f8863b0561e22fb11fc036248e7de9c6505dcc
apmanager <rfoley00@live.com>
2024-02-07 16:50:04 -0500

    updating backend var in frontend

--- src/comps/pages/dashboardComps/friends.jsx
+++ src/comps/pages/dashboardComps/friends.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import styles from '../../css/friends.module.css'
 
 const Friends = () => {
+
+
+
+  
   return (
     <div className={styles.friendsContainer}>
         <div className={styles.title}>
--- src/comps/pages/dashboardComps/settings.jsx
+++ src/comps/pages/dashboardComps/settings.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import styles from '../../css/settings.module.css'
 
 const Settings = () => {
+  const [memberSince, setMemberSince] = useState("Loading...");
+  const [dob, setDob] = useState("Loading...");
+
+  const api = ""
+
+  useEffect(() => {
+    fetch({ api })
+      .then((response) => response.json())
+      .then((data) => {
+        setMemberSince(data.memberSince);
+        setDob(data.dob)
+      });
+  }, []);
+
+
   return (
     <div className={styles.settingsContainer}>
       <div className={styles.title}>
         Settings
       </div>
       <div className={styles.membership}>
-        You have been a member since ...
+        You have been a member since {memberSince}
       </div>
       <div className={styles.dob}>
-        Your date of Birth is ...
+        Your date of Birth is {dob}
       </div>
       <div className={styles.delete} >
         Delete my account
